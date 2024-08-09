876f12e4741cfdcf3ff48997ea09fc8c71d39d9c
SPHsofi <109211098+SPHsofi@users.noreply.github.com>
2024-08-09 12:45:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const map = new Map(bracketsConfig);
+    const stack = [];
+    for(let char of str) {
+        if (map.has(char)) {
+            if (map.get(char) === char && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else {
+            for (let [open, close] of bracketsConfig) {
+                if (char === close) {
+                    if (stack.length === 0 || stack[stack.length - 1] !== open) {
+                        return false;
+                    }
+                    stack.pop();
+                }
+            }
+        }
+    }
+    return stack.length === 0;
 }
